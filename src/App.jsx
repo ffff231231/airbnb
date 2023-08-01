1b42361a28b4cfd0e03b2e6a9ea8b01f2be43590
FFFF231 <jiadenghui231@gmail.com>
2023-08-01 21:11:10 +0800

    feat: 整体布局的搭建

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import React, { memo } from 'react'
-import { useRoutes } from 'react-router-dom'
-import routes from './router'
+import React, { memo } from "react";
+import { useRoutes } from "react-router-dom";
+import routes from "./router";
+import AppHeader from "./components/app-header";
+import AppFooter from "./components/app-footer";
 
 const App = memo(() => {
   return (
-    <div className='app'>
-      <div className="header">header</div>
-      <div className="page">
-        {useRoutes(routes)}
-      </div>
-      <div className="footer">footer</div>
+    <div className="app">
+      <AppHeader />
+      <div className="page">{useRoutes(routes)}</div>
+      <AppFooter />
     </div>
-  )
-})
+  );
+});
 
-export default App
+export default App;
