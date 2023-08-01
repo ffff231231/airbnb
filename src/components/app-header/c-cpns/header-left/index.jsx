ee2190a9900eadc06f9e953a4382e6114f0f002f
FFFF231 <jiadenghui231@gmail.com>
2023-08-01 22:05:04 +0800

    feat: 添加logo

--- src/components/app-header/c-cpns/header-left/index.jsx
+++ src/components/app-header/c-cpns/header-left/index.jsx
@@ -1,10 +1,15 @@
-import React, { memo } from 'react'
-import { LeftWrapper } from './style'
+import React, { memo } from "react";
+import { LeftWrapper } from "./style";
+import IconLogo from "@/assets/svg/icon_logo";
 
 const HeaderLeft = memo(() => {
   return (
-    <LeftWrapper>HeaderLeft</LeftWrapper>
-  )
-})
+    <LeftWrapper>
+      <div className="logo">
+        <IconLogo />
+      </div>
+    </LeftWrapper>
+  );
+});
 
-export default HeaderLeft
+export default HeaderLeft;
