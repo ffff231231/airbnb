45bc3fa8d46d8565c9421f4c0b5b5a4fcab841e0
FFFF231 <jiadenghui231@gmail.com>
2023-08-03 21:44:16 +0800

    feat: Header的右侧布局和内容展示

--- src/components/app-header/c-cpns/header-right/style.js
+++ src/components/app-header/c-cpns/header-right/style.js
@@ -1,7 +1,45 @@
-import { styled } from "styled-components";
+import styled from "styled-components";
 
 export const RightWrapper = styled.div`
   flex: 1;
   display: flex;
   justify-content: flex-end;
-`
+  align-items: center;
+
+  color: ${(props) => props.theme.text.primaryColor};
+  font-size: 14px;
+  font-weight: 600;
+
+  .btns {
+    display: flex;
+
+    .btn {
+      height: 18px;
+      line-height: 18px;
+      padding: 12px 15px;
+      border-radius: 22px;
+      cursor: pointer;
+
+      &:hover {
+        background-color: #f5f5f5;
+      }
+    }
+  }
+
+  .profile {
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    width: 77px;
+    height: 42px;
+    margin-right: 24px;
+    box-sizing: border-box;
+    border: 1px solid #ccc;
+    border-radius: 25px;
+    background-color: #fff;
+    color: ${(props) => props.theme.text.primaryColor};
+    cursor: pointer;
+
+    ${(props) => props.theme.mixin.boxShadow};
+  }
+`;
